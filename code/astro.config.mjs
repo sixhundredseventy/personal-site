45415ab49ee12914f9c33ffd92e4d9e8f930405b
riyuko <58562587+stealingyourserver@users.noreply.github.com>
2022-10-09 01:20:29 +0200

    v1.0
    - removed useless files
    - installed astro.build
    - added tailwind and image from astro
    - added video background
    - added text to the front
    - added socials

---
+++ code/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image()],
+  output: "server",
+  adapter: cloudflare()
+});
