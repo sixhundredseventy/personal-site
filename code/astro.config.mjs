3205c5e90694774472f841f26bbf3cb820486b4d
riyuko <58562587+stealingyourserver@users.noreply.github.com>
2022-10-09 15:31:42 +0200

    Deno attempt

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
 import prefetch from "@astrojs/prefetch";
-
 import compress from "astro-compress";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), mdx(), prefetch(), compress()],
   output: "server",
-  adapter: vercel()
+  adapter: deno()
 });
