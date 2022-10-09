363ba4b475fe7d92e64ae24851de332732b0c488
riyuko <58562587+stealingyourserver@users.noreply.github.com>
2022-10-09 02:08:26 +0200

    v1.0.1
    - Removed the astro cloudflare module

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
-  output: "server",
-  adapter: cloudflare()
+  integrations: [tailwind(), image()]
 });
