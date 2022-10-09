7992af8c2d00fdf45d43bdd21ce9825df41c19e4
riyuko <58562587+stealingyourserver@users.noreply.github.com>
2022-10-09 14:38:53 +0200

    v1.0.3
    - Swapped out the cloudflare adapter with the one for vercel, as their ecosystem supports more features
    - Added compress and prefetch for better load speeds
    - Added MDX for future designs
    - Added sitemap for better SEO

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
+import prefetch from "@astrojs/prefetch";
 
-import cloudflare from "@astrojs/cloudflare";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap(), mdx(), prefetch(), compress()],
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
